2c54b0bc28b46faacd7f8e30553701724c8572f0
Michael S. Klishin <michael@novemberain.com>
2008-06-21 18:07:12 +0300

    Extract single job processing into a method on it's own.

--- lib/merb_mail_queue/processor.rb
+++ lib/merb_mail_queue/processor.rb
@@ -4,12 +4,15 @@ module Merb
 
       def process(queue = Merb::MailQueue.mail_queue_job_model_class.load_queue)
         queue.each do |job|
-          Merb::Mailer.new(job.attributes).deliver!
-          
-          job.destroy
+          process_job(job)
         end
       end
-    
+      
+      def process_job(job)
+        Merb::Mailer.new(job.attributes).deliver!
+          
+        job.destroy
+      end
     end
   end
 end
